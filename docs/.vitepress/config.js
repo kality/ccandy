e89e1e0f26de81198ba1eba093f0f54777b669a5
Yzy Wolf <yzy5518@hotmail.com>
2024-08-31 12:45:27 +0800

    Update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -11,10 +11,19 @@ export default {
     title: '疯狂的糖果',
     description: 'CrazyCandy',
     // 打包目录 outDir: './dist',
+
     head: [
-    // 添加图标
+    // 添加浏览器上的icon
       ['link', { rel: 'icon', href: '/ccandy/favicon.ico' }]
     ],
+
+    // 显示markdown文件中代码块的行数
+    markdown: {
+      lineNumbers: true
+    },
+
+    // 获取每个文件最后一次 git 提交的 UNIX 时间戳(ms)，同时它将以合适的日期格式显示在每一页的底部
+    lastUpdated: true, // string | boolean
 
     // 主题配置
     themeConfig: {
@@ -23,6 +32,11 @@ export default {
 
       // logo右边的标题
       siteTitle: '疯狂的糖果',
+
+      // 首页搜索框
+      search: {
+        provider: "local"
+      },
 
       // 头部导航栏配置
       nav: topNav,
@@ -33,11 +47,20 @@ export default {
         "/Write": WriteSidebar,
       },
 
+      // 右侧导航栏
+      outline: {
+        level: [2,6],
+        label: "本页目录"
+      },
+
       // 编辑链接
       editLink: {
         pattern: "https://github.com/kality/ccandy/edit/main/docs/:path", // 自己项目仓库地址
         text: "在 github 上编辑此页",
       },
+
+      // 显示最后更新时间
+      lastUpdatedText: "最后更新", // string
 
       // 站点页脚配置
       footer: {
@@ -49,8 +72,5 @@ export default {
       socialLinks: [
         { icon: 'github', link: 'https://github.com' },
       ],
-
-      // 右侧边栏配置，默认值是"In this page"
-      outlineTitle: "本页目录",
     },
  }
